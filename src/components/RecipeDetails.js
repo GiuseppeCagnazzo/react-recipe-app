0c2b1653a85bbfe1500ecae1e69e5f986226f625
GiuseppeCagnazzo <giuseppe.cagnazzo@gmail.com>
2019-07-01 00:04:22 +0200

    first commit

---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import { recipe } from "../tempDetails";
+
+export default class RecipeDetails extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     recipe: recipe,
+  //     url: `https://www.food2fork.com/api/get?key=b3d394aa6b6e827ed989b43f7acbda86&rId=${
+  //       this.props.id
+  //     }`
+  //   };
+  // }
+
+  // async componentDidMount() {
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+
+  //     this.setState({
+  //       recipe: jsonData.recipe
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  state = {
+    recipe: recipe
+  };
+
+  async componentDidMount() {
+    // console.log(this.props.id);
+    const id = this.props.id;
+    const url = `https://www.food2fork.com/api/get?key=b3d394aa6b6e827ed989b43f7acbda86&rId=${id}`;
+    try {
+      const data = await fetch(url);
+      const jsonData = await data.json();
+
+      this.setState(
+        (state, props) => {
+          return { recipe: jsonData.recipe };
+        },
+        () => {}
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    // console.log(this.state.recipe);
+    const {
+      image_url,
+      publisher,
+      publisher_url,
+      source_url,
+      title,
+      ingredients
+    } = this.state.recipe;
+
+    const { handleIndex } = this.props;
+
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 my-3">
+              <button
+                type="button"
+                className="btn btn-warning mb-5 text-capitalized"
+                onClick={() => handleIndex(1)}
+              >
+                Back to recipe list
+              </button>
+              <img src={image_url} alt="recipse" className="d-block w-100" />
+            </div>
+            {/* details section (second column) */}
+            <div className="col-10 mx-auto col-md-6 my-3">
+              <h6 className="text-uppercase">{title}</h6>
+              <h6 className="text-warned text-capitalize">
+                provided by {publisher}
+              </h6>
+              <a
+                href={publisher_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-primary mt-2 text-capitalize"
+              >
+                publisher webpage
+              </a>
+              <a
+                href={source_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-success mt-2 mx-3 text-capitalize"
+              >
+                publisher webpage
+              </a>
+              <ul className="list-group mt-4">
+                <h2 className="mt-3 mb-4">Ingredients</h2>
+                {ingredients.map((item, index) => {
+                  return (
+                    <li key={index} className="list-group-item text-slanted">
+                      {item}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
